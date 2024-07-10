10d41c274bb877d930de84096ebfa1490f3bad20
sid2417 <siddhartha.java9398@gmail.com>
2024-07-11 00:17:17 +0530

    checking source code

--- current.sh
+++ current.sh
@@ -6,6 +6,8 @@ echo "This is my current Team $VARIABLE"
 
 echo "This is my current process Id : $$"
 
+source ./other.sh
+
 echo "This is Other Team $VARIABLE"
 
 echo "This is my current process Id : $$"
